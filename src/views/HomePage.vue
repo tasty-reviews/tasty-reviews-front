509d16f3bd3f2dfe88ead0136f2db4c698261a00
gptmd97 <nhm999888@gmail.com>
2024-05-07 15:47:57 +0900

    feat: 현재위치 카테고리

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,24 +2,16 @@
     <!-- 전체 페이지 레이아웃을 나타내는 div 요소 -->
     <div class="ex-layout">
       <!-- 글로벌 메뉴를 나타내는 div 요소 -->
-      <div class="gnb"><!-- 버튼 1 -->
-        <button class="gnb-btn"><router-link :to="'/Users'">마이페이지</router-link></button>
-        <!-- 버튼 2 -->
-        <button class="gnb-btn">Button 2</button>
-        <!-- 버튼 3 -->
-        <button class="gnb-btn">Button 3</button>
-        <!-- 버튼 4 -->
-        <button class="gnb-btn">Button 4</button>
-        <!-- 버튼 5 -->
-        <button class="gnb-btn">Button 5</button>
-        <!-- 버튼 6 -->
+      <div class="gnb">
+        <button class="gnb-btn" @click="changeComponent('Main')">메인</button>
+        <button class="gnb-btn" @click="changeComponent('Ranking')">랭킹</button>
+        <button class="gnb-btn" @click="changeComponent('Favorites')">즐겨찾기</button>
+        <button class="gnb-btn" @click="changeComponent('MyPage')">마이페이지</button>
         <button class="gnb-btn"> <router-link :to="'/login'">로그인</router-link></button>
       </div>
       <!-- 왼쪽 메뉴를 나타내는 div 요소 -->
-      <div class="lnb">
-        <input type="text" v-model="searchKeyword" placeholder="검색" @keyup.enter="searchMap">
-      </div>
-      <MapComp :keyword="searchKeyword"/>
+      <div class="lnb"><router-view name="leftMenu"></router-view></div>
+      <MapComp/>
     </div>
   </template>
   
@@ -50,13 +42,11 @@
         const exLayout = document.querySelector('.ex-layout'); // ex-layout 클래스를 가진 요소 선택
         const windowHeight = window.innerHeight; // 창의 높이를 가져옴
         exLayout.style.height = `${windowHeight}px`; // 창의 높이를 요소의 높이로 설정
+      },
+      changeComponent(componentName) {
+      this.$router.push({ name: componentName });
       }
-    },
-    data(){
-      return {
-        searchKeyword:'', // 검색어 저장
-      };
-    }  
+    }
   }
   </script>
   
@@ -73,38 +63,28 @@
     height: 100vh; /* 화면 전체 높이를 사용하도록 설정 */
   }
   .ex-layout .gnb {
+    border-right: 1px solid rgb(172, 172, 172);
     float: left;
     width: 80px;
     padding: 10px;
-    background-color: skyblue;
+    background-color: white;
   }
   /* 왼쪽 메뉴의 스타일 설정 */
   .ex-layout .lnb {
-  float: left;
-  padding: 15px;
-  width: 250px; /* 너비 조정 */
-  background-color: orange;
-  display: flex; /* 내부 요소를 flex로 배치 */
-  flex-direction: column; /* 자식 요소를 세로로 배치 */
-  align-items: start; /* 자식 요소를 왼쪽 정렬 */
-}
-  /* 검색창 스타일 조정 */
-  input[type="text"] {
-  width: 100%; /* 검색창 너비를 .lnb에 맞춤 */
-  padding: 8px;
-  margin-bottom: 10px; /* 검색창 아래에 여백 추가 */
-  border: 1px solid #ccc;
-  border-radius: 5px;
-}
+    overflow: scroll;
+    float: left;
+    padding: 15px;
+    width: 500px;
+    background-color: white;
+  }
   .gnb-btn {
+    border: 1px solid rgb(172, 172, 172);
     width: 100%;
     height: 40px;
     border-radius: 10px;
-    border: none; 
     background-color: white;
     margin-bottom: 20px;
   }
-
   </style>
   
   
