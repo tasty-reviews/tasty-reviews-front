261afde62cb92d4c87657483c0ee6a6268e27edb
gptmd97 <nhm999888@gmail.com>
2024-05-30 10:21:21 +0900

    Feat: 상세정보구현

---
+++ src/components/main-Pages/Main-Page/StoreComp.vue
@@ -0,0 +1,137 @@
+<template>
+      <!-- 가게 정보를 표시하는 각각의 요소 -->
+      <div v-for="(store, index) in stores" :key="index" class="store-info">
+        <div class="first-line"> 
+          <!-- 가게 이름 -->
+          <h3 @click="goToDetail(store.id)" class="clickable">{{ store.place_name }}</h3>
+          <!-- 카테고리 -->
+          <p class="category">{{ store.category }}</p>
+           <!-- 별점 -->
+          <p class="fl-right">⭐️ {{ store.rating }}</p>
+        </div>
+        <div class="second-line">
+          <!-- 주소 -->
+          <p> 주소: {{ store.address_name }}</p>
+          <!-- 리뷰 갯수 -->
+          <p class="fl-right">리뷰 갯수: {{ store.reviewCount }}</p>
+        </div>
+        <!-- 사진 -->
+        <img :src="store.imageUrl" alt="가게 사진">
+      </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  import { mapState} from 'vuex'; // Vuex에서 mutations를 사용하기 위해 import
+
+  export default {
+    data() {
+      return {
+        stores: [] // 받아온 가게 정보를 저장할 배열
+      };
+    },
+    computed: {
+      ...mapState(['enteredSearchWord','selectedSubRegion']) // selectedSubRegion 상태를 컴포넌트의 computed 속성으로 매핑
+    },
+    mounted() {
+      // 컴포넌트가 마운트되면 가게 정보를 가져오는 메서드 호출
+      this.getStores();
+    },
+    
+    methods: {
+    goToDetail(storeId) {
+      this.$router.push({ name: 'StoreDetail', params: { id: storeId } });
+    },
+      
+      async getStores() {
+        try {
+          // 선택된 하위 지역에 해당하는 검색어 가져오기
+          const searchQuery = this.enteredSearchWord ? this.enteredSearchWord : this.selectedSubRegion || '포차';
+          // 백엔드에서 가게 정보를 가져오는 HTTP 요청 보내기
+          const response = await axios.get(`http://localhost:8080/search?q=${searchQuery}`);
+          // 받은 응답을 컴포넌트의 데이터에 저장
+          this.stores = response.data.documents;
+
+          // 각 가게의 place_name을 사용하여 네이버 이미지 검색 API에 요청을 보내고 이미지 URL을 가져와 저장
+          await Promise.all(this.stores.map(async (store) => {
+            const imageUrl = await this.getNaverImage(store.place_name);
+            store.imageUrl = imageUrl;
+          }));
+          // 받은 stores 배열을 로그에 출력
+          console.log('Stores:', this.stores);
+
+        } catch (error) {
+          console.error('가게 정보를 가져오는 도중 오류 발생:', error);
+        }
+      },
+      // 클라이언트에서 네이버 API를 호출하는 함수
+      async getNaverImage(query) {
+      try {
+        const response = await axios.get('http://localhost:8080/search/image', {
+          params: {
+            query: query
+          }
+        });
+        // 성공적으로 데이터를 받아온 경우 처리
+        console.log('네이버 이미지 검색 결과:', response.data);
+        // 첫 번째 이미지의 링크만 반환
+        return response.data.link; 
+      } catch (error) {
+        // 오류 발생 시 처리
+        console.error('네이버 이미지 검색 도중 오류 발생:', error);
+        return null;
+      }
+    }
+  }
+};
+
+  </script>
+  
+  <style scoped>
+  /* 공통 스타일 */
+  .store-info {
+    margin-bottom: 15px;
+    padding: 10px;
+    border-radius: 8px;
+    box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
+    background-color: #f9f9f9;
+    font-size: 15px; /* 텍스트의 폰트 사이즈를 조정하세요 */
+    font-family: Arial, sans-serif;
+    border-radius: 8px; /* 모서리를 둥글게 만듦 */
+    border: none; /* 기존의 테두리 제거 */
+    background-color: white; /* 배경색을 흰색으로 설정 */
+    box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1), 0 2px 10px rgba(0, 0, 0, 0.1); /* 그림자 추가 */
+  }
+  /* 가게 이름 스타일 */
+  .store-info h3 {
+    font-size: 20px;
+  }
+  /* 사진 스타일 */
+  .store-info img {
+    width: 100%;
+    height:150px;
+    border-radius: 8px;
+    margin-top: 10px;
+    border-radius: 10px; /* 모서리를 둥글게 만듦 */
+    border: none; /* 기존의 테두리 제거 */
+    background-color: white; /* 배경색을 흰색으로 설정 */
+    box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1), 0 2px 10px rgba(0, 0, 0, 0.1); /* 그림자 추가 */
+  }
+  .first-line {
+    font-size:11px;
+    display: flex;
+    align-items: center;
+  }
+  .category {
+    color: gray;
+  }
+  .fl-right{
+    margin-left: auto;
+  }
+  .second-line{
+    font-size:13px;
+    display: flex;
+    align-items: center;
+  }
+  </style>
+  
--- src/router/index.js
+++ src/router/index.js
@@ -18,140 +18,76 @@ import FindPasswordPage from '@/views/FindPasswordPage.vue'
 import StoreDetail from '../components/main-Pages/Main-Page/Store-Page/StoreDetail'
 import StoreDetail from '../components/main-Pages/Main-Page/Store-Page/StoreDetail'
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: HomePage,
-    children: [
-      {
-        path: '', // 부모 컴포넌트의 기본 경로
-        name: 'Main',
-        components: {
-          leftMenu: MainPage // MainPage를 leftMenu 영역에 렌더링
-        }
-      },
-      {
-        path: '/store/:id', // 부모 컴포넌트의 기본 경로
-        name: 'StoreDetail',
-        components: {
-          leftMenu: StoreDetail // MainPage를 leftMenu 영역에 렌더링
-        }
-      },
-      {
-        path: 'searchbox', // 부모 컴포넌트의 기본 경로
-        name: 'SearchBox',
-        components: {
-          leftMenu: SearchPage // MainPage를 leftMenu 영역에 렌더링
-        }
-      },
-      {
-        path: 'ranking',
-        name: 'Ranking',
-        components: {
-          leftMenu: RankingPage // RankingPage를 leftMenu 영역에 렌더링
-        }
-      },
-      {
-        path: 'favorites',
-        name: 'Favorites',
-        components: {
-          leftMenu: FavoritesPage // FavoritesPage를 leftMenu 영역에 렌더링
-        }
-      },
-      {
-        path: 'mypage',
-        name: 'MyPage',
-        components: {
-          leftMenu: MyPage // MyPage를 leftMenu 영역에 렌더링
-        },
-        meta: { requiresAuth: true } // 인증이 필요한 경로
-      },
-      {
-        path: 'fixing',
-        name: 'Fixing',
-        components: {
-          leftMenu: FixingPage // FixingPage를 leftMenu 영역에 렌더링
-        },
-        meta: { requiresAuth: true } // 인증이 필요한 경로
-      },
-      {
-        path: 'nick-change', // 새로운 닉네임 변경 경로 추가
-        name: 'NickChange',
-        components: {
-          leftMenu: NicknameChangePage
-        },
-        meta: { requiresAuth: true } // 인증이 필요한 경로
-      },
-      {
-        path: 'pass-change',
-        name: 'PassChange',
-        components: {
-          leftMenu: PasswordChangePage
-        },
-        meta: { requiresAuth: true } // 인증이 필요한 경로
-      }
-    ] 
-  },
-  {
-    path: '/login',
-    name: 'LogIn',
-    component: LogInPage
-  },
-  {
-    path: '/signup',
-    name: 'SignUp',
-    component: SignUpPage
-  },
-  {
-    path: '/find-password',
-    name: 'FindPassword',
-    component: FindPasswordPage
-  }
-];
-
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-});
-
-router.beforeEach((to, from, next) => {
-  // 인증이 필요한 페이지인지 확인
-  if (to.matched.some(record => record.meta.requiresAuth)) {
-    // 쿠키 문자열을 가져옴
-    const cookieString = document.cookie;
-
-    // 쿠키 문자열이 비어있는지 확인
-    if (cookieString) {
-      // 쿠키 문자열을 세미콜론과 공백을 기준으로 분할하여 배열로 변환
-      const cookies = cookieString.split('; ');
-
-      // 'refresh='로 시작하는 쿠키를 찾아서 해당 쿠키의 값을 가져옴
-      const refreshCookie = cookies.find(cookie => cookie.startsWith('refresh='));
-
-      // 'refresh='로 시작하는 쿠키가 존재하는지 확인
-      if (refreshCookie) {
-        // 등호('=')를 기준으로 쿠키를 분할하여 값을 가져옴
-        const refreshValue = refreshCookie.split('=')[1];
-
-        // refreshValue를 사용하여 추가적인 작업 수행
-        console.log('Refresh token:', refreshValue);
-
-        // refresh 토큰이 있는 경우 페이지로 이동
-        next();
-      } else {
-        // 'refresh='로 시작하는 쿠키가 없는 경우 로그인 페이지로 리디렉션
-        next({ name: 'LogIn' });
-      }
-    } else {
-      // 쿠키 문자열이 없는 경우 로그인 페이지로 리디렉션
-      next({ name: 'LogIn' });
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: HomePage,
+      children: [
+        {
+          path: '', // 부모 컴포넌트의 기본 경로
+          name: 'Main',
+          components: {
+            leftMenu: MainPage, // MainPage를 leftMenu 영역에 렌더링
+          }
+        },
+        {
+          path: 'searchbox', // 부모 컴포넌트의 기본 경로
+          name: 'SearchBox',
+          components: {
+            leftMenu: SearchPage // MainPage를 leftMenu 영역에 렌더링
+          }
+        },
+        {
+          path: 'ranking',
+          name: 'Ranking',
+          components: {
+            leftMenu: RankingPage // RankingPage를 leftMenu 영역에 렌더링
+          }
+        },
+        {
+          path: 'favorites',
+          name: 'Favorites',
+          components: {
+            leftMenu: FavoritesPage // FavoritesPage를 leftMenu 영역에 렌더링
+          }
+        },
+        {
+          path: 'mypage',
+          name: 'MyPage',
+          components: {
+            leftMenu: MyPage // MyPage를 leftMenu 영역에 렌더링
+          }
+        },
+        {
+          path: 'storedetail/:id',
+          name: 'StoreDetail',
+          components: {
+            leftMenu: StoreDetail
+          }
+        }
+        
+      ] },
+    {
+      path: '/login',
+      name: 'LogIn',
+      component: LogInPage
+    },
+    {
+      path: '/signup',
+      name: 'SignUp',
+      component: SignUpPage
     }
-  } else {
-    // 인증이 필요 없는 페이지는 그대로 진행
-    next();
-  }
-});
+    ,
+    {
+      path: '/find-password',
+      name: 'FindPassword',
+      component: FindPasswordPage
+    }
+  ]
+})
 
 export default router;
 export default router;
