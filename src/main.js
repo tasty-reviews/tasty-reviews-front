099066efe7ebe38930e38b4717a8bba72c20d339
wodudam <wodudam@naver.com>
2024-05-31 20:48:48 +0900

    feat: 사이드바 이미지 삽입

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store';
 
+// Font Awesome 설정
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faHome, faMap, faStar, faMapMarkerAlt, faUser } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faHome, faMap, faStar, faMapMarkerAlt, faUser)
+
 const app = createApp(App)
 
 app.use(router).use(store).mount('#app'); // 스토어를 앱에 적용
