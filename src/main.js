ba879f501bf8fb0af8699b788d327672963a6b27
wodudam <wodudam@naver.com>
2024-05-31 21:06:58 +0900

    fix: 오류 수정

--- src/main.js
+++ src/main.js
@@ -4,11 +4,11 @@ import router from './router'
 import store from './store';
 
 // Font Awesome 설정
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faHome, faMap, faStar, faMapMarkerAlt, faUser } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { faHome, faMap, faStar, faMapMarkerAlt, faUser } from '@fortawesome/free-solid-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faHome, faMap, faStar, faMapMarkerAlt, faUser)
+// library.add(faHome, faMap, faStar, faMapMarkerAlt, faUser)
 
 const app = createApp(App)
 
