36bdc06a1e1b08f30f7824e78d140ef27df621f4
DongYeop <s01054830455@gmail.com>
2024-06-01 14:58:25 +0900

    refactor: 사이드바 코드 정리
    - 신규코드로 업데이트 구 버전 코드 삭제

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="ex-layout">
     <div class="gnb">
-
       <button class="gnb-btn" @click="changeComponent('Main')">
         <img src="@/assets/home.png" class="icon"/>
         <span>메인</span>
@@ -10,7 +9,7 @@
         <img src="@/assets/star.png" class="icon"/>
         <span>랭킹</span>
       </button>
-      <button class="gnb-btn" @click="changeComponent('Favorites')">
+      <button class="gnb-btn" @click="changeComponent('MyMaps')">
         <img src="@/assets/mymap.png" class="icon"/>
         <span>내지도</span>
       </button>
@@ -18,13 +17,6 @@
         <img src="@/assets/user.png" class="icon"/>
         <span>마이</span>
       </button>
-
-      <button class="gnb-btn" @click="changeComponent('Main')">메인</button>
-      <button class="gnb-btn" @click="changeComponent('Ranking')">랭킹</button>
-      <button class="gnb-btn" @click="changeComponent('MyMaps')">내지도</button>
-      <button class="gnb-btn" @click="isLoggedIn ? changeComponent('MyPage') : changeComponent('LogIn')">마이페이지</button>
-      <!-- <button class="gnb-btn" @click="changeComponent('LogIn')">로그인</button> -->
-      <!-- 로그인 유무에 따라 마이페이지 버튼에서 동작하도록 할 예정 -->
 
     </div>
     <div class="lnb"><router-view name="leftMenu"></router-view></div>
@@ -38,21 +30,18 @@ import MapComp from '../components/comp-Map/MapComp.vue';
 import { mapState } from 'vuex';
 
 export default {
-  computed: {
-    ...mapState(['isLoggedIn'])
-  },
+  name: 'HomePage',
   components: {
     MapComp
   },
-
-  name: 'HomePage',
-  data(){
+  computed: {
+    ...mapState(['isLoggedIn'])
+  },
+  data() {
+    return {
+      // Add your data properties here if needed
     };
   },
-
-
-  // Vue 인스턴스가 마운트되었을 때 실행되는 함수
-
   mounted() {
     this.adjustHeight();
     window.addEventListener('resize', this.adjustHeight);
@@ -69,18 +58,16 @@ export default {
     changeComponent(componentName) {
       this.$router.push({ name: componentName });
     },
-    goToCurrentLocation(){
-      if (this.$refs.mapComp){
+    goToCurrentLocation() {
+      if (this.$refs.mapComp) {
         this.$refs.mapComp.goToCurrentLocation();
         console.log("Navigating to current location");
       } else {
         console.error('MapComp is not available');
-      } 
+      }
     }
   }
-
-  }
-}
+};
 </script>
 
 <style>
@@ -154,16 +141,16 @@ export default {
   background-color: white;
   margin-bottom: 20px;
 }
- /*카테고리메뉴버튼*/
+/*카테고리메뉴버튼*/
 .current-location-btn {
-position: absolute;
-top: 20px; /* 상단에서 일정 거리에 위치 */
-padding: 10px 20px;
-background-color: #fff;
-border: 1px solid #ccc;
-border-radius: 5px;
-cursor: pointer;
-z-index: 1000;
+  position: absolute;
+  top: 20px; /* 상단에서 일정 거리에 위치 */
+  padding: 10px 20px;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  cursor: pointer;
+  z-index: 1000;
 }
 
 .category-btn {
